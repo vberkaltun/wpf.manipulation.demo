fd310f2b9990b5145e4188e1a408c1242b715753
Berk Altun <berk.altun@applusrtd.com>
2024-06-04 16:52:38 +0200

    Test exporting

---
+++ ex/por/ter/Exporter/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exporter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create() creates a file at pathName 
+            FileStream fs = File.Create(args[0]);
+        }
+    }
+}
